1e94b87a3e8b44c0a0e20d9e66aad08de596322a
Secret <i@wangmao.me>
2023-03-13 16:02:44 +0800

    update home style

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ header("Content-Type: text/html");
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
     <title>短链接生成</title>
     <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <style>
