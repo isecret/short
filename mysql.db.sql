550d148fc01e3685b6d3273cd6b8aa068e6eb285
Secret <i@wangmao.me>
2023-03-16 10:22:04 +0800

    modify IP field length to support IPv6

--- mysql.db.sql
+++ mysql.db.sql
@@ -3,7 +3,7 @@ CREATE TABLE `url` (
   `code` varchar(32) NOT NULL,
   `hash` varchar(32) NOT NULL,
   `url` text NOT NULL,
-  `ip` varchar(32) NOT NULL,
+  `ip` varchar(64) NOT NULL,
   `user_agent` text,
   `visit_count` int(11) NOT NULL DEFAULT 0,
   `create_date` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -15,7 +15,7 @@ CREATE TABLE `url` (
 CREATE TABLE `visit_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url_id` int(11) NOT NULL,
-  `ip` varchar(32) NOT NULL,
+  `ip` varchar(64) NOT NULL,
   `user_agent` text,
   `create_date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
